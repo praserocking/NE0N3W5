b9fc5a4d763f73205e2dd79a6763350615d31f0c
prasanna <praserocking@gmail.com>
2015-12-19 19:29:24 +0530

    first commit - created model User

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neonews::Application.config.secret_token = '6ab22520998da0ccfbab225707962c66c64a212f3ecca8550af5925e9324f4967a98e561287db752af94507a603a5908c5a53f8152b54fe6f8adb1a65633e516'
