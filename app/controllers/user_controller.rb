49c1d32ce60d3ffdd8c51c9d266bd2baa367877a
prasanna <praserocking@gmail.com>
2015-12-20 13:59:20 +0530

    added login methods working

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,4 +18,17 @@ class UserController < ApplicationController
 	def login
 		@page_title = "Login"
 	end
+
+	def login_handler
+		logged_in_user = User.where(email:params[:email]).first
+
+		if !logged_in_user.blank? and logged_in_user.authenticate(params[:password])
+			flash[:success] = "User Logged in"
+			render "login"
+		else
+			@errors = ["Email or Password is wrong"]
+			@page_title = "Login"
+			render "login"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Neonews::Application.routes.draw do
   get "/signup" => "User#signup"
   post "/signup"=> "User#signup_handler"
+
+  get "/login" => "User#login"
+  post "/login"=> "User#login_handler"
 end
