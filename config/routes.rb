b30c2edc3aa89896a2664852122f4f19a11fe0b9
praserocking <praserocking@gmail.com>
2015-12-21 03:50:42 +0530

    another shittic typo bug

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Neonews::Application.routes.draw do
   get "/login" => "User#login"
   post "/login"=> "User#login_handler"
 
-  get "/logout" => "User#logout_handle"
+  get "/logout" => "User#logout_handler"
 
   get "/" => "Post#show_posts"
 end
