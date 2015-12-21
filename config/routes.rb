867db1e6aadd0177414996d3b401a5c198b2c5ec
praserocking <praserocking@gmail.com>
2015-12-22 02:01:42 +0530

    fixing ambiguity in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Neonews::Application.routes.draw do
   get "/logout" => "User#logout_handler"
 
   get "/" => "Post#show_posts"
-  get "/:pageno" => "Post#show_posts"
+  get "/posts/:pageno" => "Post#show_posts"
 
   get "/newpost" => "Post#create_post"
   post"/newpost" => "Post#create_post_handler"
